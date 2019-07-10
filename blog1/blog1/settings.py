f3d5365c172fbdd5b16c11a317fa8071d8ab035a
lklo <1269100650@qq.com>
2019-07-10 09:03:23 +0800

    使用xadmin

--- blog1/blog1/settings.py
+++ blog1/blog1/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "blog",
-    "comment"
+    "comment",
+    "xadmin",
+    "crispy_forms"
 ]
 
 MIDDLEWARE = [
